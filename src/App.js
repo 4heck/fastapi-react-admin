f6ed35e1fa0dc4e6bd119979a38c1692f64e0df0
Rishat Sharafiev <rishatsharafiev@ya.ru>
2020-10-15 10:25:43 +0300

    Basic ui

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import { Admin, Resource } from 'react-admin';
+import jsonServerProvider from 'ra-data-json-server';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import { UserList, UserCreate, UserShow } from './Users';
+
+const App = () => (
+  <Admin dataProvider={jsonServerProvider('http://127.0.0.1:9000')}>
+    < Resource name="users" list={UserList} create={UserCreate} show={UserShow} />
+  </Admin >
+);
 
 export default App;
